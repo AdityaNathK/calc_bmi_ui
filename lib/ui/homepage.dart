3fea61cd8a138749d7f1bb6c9552a7c047d60fce
kronosking007 <aditya.k.nath@gmail.com>
2019-05-15 06:35:40 +1000

    Second commit : added Height Details - Partially implemented

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './widget_utils.dart';
 import './gender_card.dart';
 import './weight_card.dart';
+import './height_card.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -53,7 +54,7 @@ class HomePage extends StatelessWidget {
           left: 14.0, right: 14.0, top: screenAwareSize(32.0, context)),
       child: Row(
         children: <Widget>[
-          Expanded(child: _cardDisplay("Height")),
+          Expanded(child: HeightCard()),
           Expanded(
             child: Column(
               children: <Widget>[
