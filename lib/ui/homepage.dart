6d4b66d0cd35796d0d6696e614b8ad8473dd45dc
kronosking007 <aditya.k.nath@gmail.com>
2019-05-15 08:41:16 +1000

    Fourth commit : Full Functionality implemented. No Errors. => Result Page and Splash Screen next

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,43 +1,70 @@
-import 'package:flutter/material.dart';
-import './widget_utils.dart';
+import './fade_route.dart';
 import './gender_card.dart';
-import './weight_card.dart';
 import './height_card.dart';
+import './pacman_slider.dart';
+import './transition_dot.dart';
+import './weight_card.dart';
 import './gender_model.dart';
-import './input_summary_card.dart';
+import './result_page.dart';
+import './widget_utils.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  InputPageState createState() {
-    return new InputPageState();
+  HomePageState createState() {
+    return new HomePageState();
   }
 }
 
-class InputPageState extends State<HomePage> {
+class HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController _submitAnimationController;
   Gender gender = Gender.other;
-  int height = 170;
+  int height = 180;
   int weight = 70;
+
+  @override
+  void initState() {
+    super.initState();
+    _submitAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _submitAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _goToResultPage().then((_) => _submitAnimationController.reset());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _submitAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: MediaQuery.of(context).padding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _buildTitle(context),
-            InputSummaryCard(
-              gender: gender,
-              weight: weight,
-              height: height,
-            ),
-            Expanded(child: _middleArea(context)),
-            _bottomArea(context),
-          ],
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildTitle(context),
+              InputSummaryCard(
+                gender: gender,
+                weight: weight,
+                height: height,
+              ),
+              Expanded(child: _buildCards(context)),
+              _buildBottom(context),
+            ],
+          ),
         ),
-      ),
+        TransitionDot(animation: _submitAnimationController),
+      ],
     );
-  } // end of Build Context
+  }
 
   Widget _buildTitle(BuildContext context) {
     return Padding(
@@ -45,15 +72,50 @@ class InputPageState extends State<HomePage> {
         left: 24.0,
         top: screenAwareSize(56.0, context),
       ),
-      child: Text(
-        "BMI Calculator",
-        style: new TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+      child: Center(
+        child: Text(
+          "BMI CALCULATOR",
+          style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
 
-// Bottom Part **********/
-  Widget _bottomArea(BuildContext context) {
+  Widget _buildCards(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: HeightCard(
+            height: height,
+            onChanged: (val) => setState(() => height = val),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: WeightCard(
+                  weight: weight,
+                  onChanged: (val) => setState(() => weight = val),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+              ),
+              Expanded(
+                child: GenderCard(
+                  gender: gender,
+                  onChanged: (val) => setState(() => gender = val),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottom(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
         left: screenAwareSize(16.0, context),
@@ -61,56 +123,77 @@ class InputPageState extends State<HomePage> {
         bottom: screenAwareSize(22.0, context),
         top: screenAwareSize(14.0, context),
       ),
-      child: Placeholder(
-        fallbackHeight: screenAwareSize(52.0, context),
-        color: Theme.of(context).primaryColor,
+      child: PacmanSlider(
+        submitAnimationController: _submitAnimationController,
+        onSubmit: onPacmanSubmit,
       ),
     );
   }
 
-// Bottom Part **********/
+  void onPacmanSubmit() {
+    _submitAnimationController.forward();
+  }
 
-// Middle Part ********/
-  Widget _middleArea(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          left: 14.0, right: 14.0, top: screenAwareSize(32.0, context)),
-      child: Row(
-        children: <Widget>[
-          Expanded(child: HeightCard(
+  _goToResultPage() async {
+    return Navigator.of(context).push(FadeRoute(
+      builder: (context) => ResultPage(
+            weight: weight,
             height: height,
-            onChanged: (val) => setState(() => height = val),
-          ),),
-          Expanded(
-            child: Column(
-              children: <Widget>[
-                Expanded(child: WeightCard(
-                  weight: weight,
-                  onChanged: (val) => setState(() => weight = val),
-                )),
-                Expanded(child: GenderCard(
-                  gender: gender,
-                  onChanged: (val) => setState(() => gender = val),
-                ),),
-              ],
-            ),
+            gender: gender,
           ),
-        ],
-      ),
-    );
+    ));
   }
+}
 
-  // Middle Part
+class InputSummaryCard extends StatelessWidget {
+  final Gender gender;
+  final int height;
+  final int weight;
 
-  // Card Display Function
-  Widget _cardDisplay(String title) {
+  const InputSummaryCard({Key key, this.gender, this.height, this.weight})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Text(title),
+      margin: EdgeInsets.all(screenAwareSize(16.0, context)),
+      child: SizedBox(
+        height: screenAwareSize(32.0, context),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: _genderText()),
+            _divider(),
+            Expanded(child: _text("${weight}kg")),
+            _divider(),
+            Expanded(child: _text("${height}cm")),
+          ],
+        ),
       ),
     );
   }
-// Card Display Function
+
+  Widget _genderText() {
+    String genderText = gender == Gender.other
+        ? '-'
+        : (gender == Gender.male ? 'Male' : 'Female');
+    return _text(genderText);
+  }
+
+  Widget _text(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Color.fromRGBO(143, 144, 156, 1.0),
+        fontSize: 15.0,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _divider() {
+    return Container(
+      width: 1.0,
+      color: Color.fromRGBO(151, 151, 151, 0.1),
+    );
+  }
 }
